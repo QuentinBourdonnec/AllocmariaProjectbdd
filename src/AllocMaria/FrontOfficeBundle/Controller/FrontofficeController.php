beea598008286b781b22f793eb1e0caaa03be89d
QuentinBourdonnec <quentin.bourdonnec@laposte.net>
2019-02-11 17:13:38 +0100

    2

--- src/AllocMaria/FrontOfficeBundle/Controller/FrontofficeController.php
+++ src/AllocMaria/FrontOfficeBundle/Controller/FrontofficeController.php
@@ -10,6 +10,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use AllocMaria\FrontOfficeBundle\Entity\UserBureauAssociation;
 use AllocMaria\FrontOfficeBundle\Entity\Adherent;
 use AllocMaria\FrontOfficeBundle\Entity\ActiviteSection;
+use AllocMaria\FrontOfficeBundle\Entity\Section;
+use AllocMaria\FrontOfficeBundle\Entity\TypeUser;
 use AllocMaria\FrontOfficeBundle\Form\AdherentType;
 use Symfony\Component\HttpFoundation\Session\Session;
 
@@ -18,101 +20,128 @@ class FrontofficeController extends Controller
     public function accueilAction(Request $request)
     {
 
-        // creates a task and gives it some dummy data for this example
-        /*$task = new UserBureauAssociation();
-
-        $form = $this->createFormBuilder($task)
-            ->add('identifiantUser', TextType::class)
-            ->add('mdpUser', TextType::class)
-            ->add('save', SubmitType::class, ['label' => 'Connexion'])
-            ->getForm();
-
-        return $this->render('AllocMariaFrontOfficeBundle:FrontOffice:accueil.html.twig', [
-            'form' => $form->createView(),
-        ]);*/
-
         $em = $this->getDoctrine()->getManager();
 
-
+        //Formulaire de connexion au bureau des sections
         $form = $this->createFormBuilder()
             ->add('identifiant_section', TextType::class)
             ->add('mdp_section', TextType::class)
             ->getForm();
-
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
+        //Formulaire de connexion au bureau des encaissement
+        $form2 = $this->createFormBuilder()
+            ->add('identifiant_encaissement', TextType::class)
+            ->add('mdp_encaissement', TextType::class)
+            ->getForm();
+        $form2->handleRequest($request);
+
+        //Si l'utilisateur à cliquer sur le bouton connexion pour accéder au bureau des sections
+        if ($form->isSubmitted() && $form->isValid()) {
+            echo "form1";
+            //On récupère l'identifiant et le mot de passe
             $identifiant_section = $form["identifiant_section"]->getData();
             $mdp_section = $form["mdp_section"]->getData();
 
-
             $submit = $request->request->get('action');
 
-            //Si le bouton qui a été appuyé est celui avec la valeur 'Toutes les villes'
             if ($submit == 'Connexion') {
-                //$query = $em->createQuery("SELECT user FROM AllocMariaFrontOfficeBundle:UserBureauAssociation user WHERE user.identifiantUser=:identifiant_section AND user.mdpUser=:mdp_section")->setParameter('identifiant_section', '%' . $identifiant_section . '%', 'mdp_section', '%' . $section . '%');
-                //$query = $em->createQuery("SELECT user FROM AllocMariaFrontOfficeBundle:UserBureauAssociation user WHERE user.identifiantUser=:identifiant_section")->setParameter('identifiant_section', $identifiant_section);
-                //$query1 = $em->createQuery("SELECT user FROM AllocMariaFrontOfficeBundle:UserBureauAssociation user WHERE user.mdpUser=:mdp_section")->setParameter('mdp_section', $section);
-                //$query = $em->createQuery("SELECT user FROM AllocMariaFrontOfficeBundle:UserBureauAssociation user");
-
+                //On récupère les informations de l'utilisateur en question
                 $query = $em->createQuery("SELECT user FROM AllocMariaFrontOfficeBundle:UserBureauAssociation user WHERE user.identifiantUser=:identifiant_section AND user.mdpUser=:mdp_section");
                 $query->setParameters(array(
                     'identifiant_section'=> $identifiant_section,
                     'mdp_section'=> $mdp_section));
                 $res=$query->getResult();
-                $prenom = $res[0]->getLibelleTypeUser()->getLibelleTypeUser();
-                echo $prenom;
 
-                $query2 = $em->createQuery("SELECT 'IDENTITY(user.libelleTypeUser)' from AllocMariaFrontOfficeBundle:UserBureauAssociation user where user.identifiantUser=:identifiant_section AND user.mdpUser=:mdp_section");
-                $query2->setParameters(array(
-                    'identifiant_section'=> $identifiant_section,
-                    'mdp_section'=> $mdp_section));
-                $res2=$query2->getResult();
-
-
-
-                //$res1=$query1->getResult();
-
-                //$res = isset($res) ? $res : NULL;
-                //$res1 = isset($res1) ? $res1 : NULL;
-
-                $test=1;
-                if($prenom == 'Encaissement'){
-
-                    //$_SESSION['prenom'] = $res[''];
-                    //$res=1;
-                    //return $this->redirectToRoute('espace_bureau_sections_alloc_maria_front_office_homepage');
-                    //return $this->render('AllocMariaFrontOfficeBundle:FrontOffice:accueil.html.twig',array("ress"=>$res, "form" => $form->createView()));
-
-
-                    return $this->render('AllocMariaFrontOfficeBundle:FrontOffice\BureauDesSections:BureauSections.html.twig',array("ress"=>$res));
+                if(count($res) != 0){
+                    $libelle = $res[0]->getLibelleTypeUser()->getLibelleTypeUser();
                 }
-                else
-                    return $this->redirectToRoute('espace_bureau_sections_ma_section_alloc_maria_front_office_homepage');
-                    //return $this->render('AllocMariaFrontOfficeBundle:FrontOffice:accueil.html.twig',array("form" => $form->createView(),));
-                //$res = $query->getResult();
+                else{
+                    $libelle=NULL;
+                }
+
+                if($libelle === 'Responsable section'){
+
+                    //Gestion de la session
+                    if(!isset($_SESSION))
+                    {
+                        session_start();
+                    }
+                    $_SESSION["id_user"]=$res[0]->getIdUser();
+                    $_SESSION["nom_user"]=$res[0]->getNomUser();
+                    $_SESSION["prenom_user"]=$res[0]->getPrenomUser();
+                    $_SESSION["identifiant_user"]=$res[0]->getIdentifiantUser();
+                    $_SESSION["mdp_user"]=$res[0]->getMdpUser();
+                    $_SESSION["libelle_activite_user"]=$libelle;
+                    //$_SESSION["nom_section"]=$res[0]->getLibelleTypeUser()->getLibelleTypeUser();
+                    $_SESSION['connected'] = true;
+
+                    echo $libelle;
+                    //return $this->render('AllocMariaFrontOfficeBundle:FrontOffice\BureauDesSections:BureauSections.html.twig',array("ress"=>$res));
+                    return $this->render('AllocMariaFrontOfficeBundle:FrontOffice\BureauDesSections:BureauSections.html.twig',array('session'=> $_SESSION));
+
+                    //return $this->redirectToRoute('espace_bureau_sections_alloc_maria_front_office_homepage',array("ress"=>$res));
+                }
+                elseif($libelle != NULL && $libelle != 'Responsable section'){
+                    echo "nok";
+                    return $this->render('AllocMariaFrontOfficeBundle:FrontOffice:accueil.html.twig',array("form" => $form->createView(), "form2" => $form2->createView()));
+                }
+                else{
+                    echo "ok";
+                    return $this->render('AllocMariaFrontOfficeBundle:FrontOffice:accueil.html.twig',array("form" => $form->createView(), "form2" => $form2->createView()));
+                }
             }
 
-            /*elseif ($submit == 'identifiant_section ') {
-                $query = $em->createQuery("SELECT adherent FROM AllocMariaFrontOfficeBundle:Adherent adherent WHERE adherent.prenomAdherent LIKE :recherche")->setParameter('identifiant_section', '%' . $identifiant_section . '%');
-            }*/
+        }
+        elseif($form2->isSubmitted() && $form2->isValid()){
+            echo "form2";
+            //On récupère l'identifiant et le mot de passe
+            $identifiant_encaissement = $form2["identifiant_encaissement"]->getData();
+            $mdp_encaissement = $form2["mdp_encaissement"]->getData();
 
+            $submit = $request->request->get('action');
 
-            //return $this->render('AllocMariaFrontOfficeBundle:FrontOffice\BureauDesSections:ListeAdherent.html.twig',array("adherents"=>$res, "form" => $form->createView()));
-        }else
-            $res = 0;
-            return $this->render('AllocMariaFrontOfficeBundle:FrontOffice:accueil.html.twig',array("ress"=>$res, "form" => $form->createView()));
+            if ($submit == 'Connexion') {
 
+                $query = $em->createQuery("SELECT user FROM AllocMariaFrontOfficeBundle:UserBureauAssociation user WHERE user.identifiantUser=:identifiant_encaissement AND user.mdpUser=:mdp_encaissement");
+                $query->setParameters(array(
+                    'identifiant_encaissement'=> $identifiant_encaissement,
+                    'mdp_encaissement'=> $mdp_encaissement));
+                $res=$query->getResult();
 
+                if(count($res) != 0){
+                    $libelle = $res[0]->getLibelleTypeUser()->getLibelleTypeUser();
+                }
+                else{
+                    $libelle=NULL;
+                }
 
+                if($libelle === 'Encaissement'){
+                    echo $libelle;
+                    return $this->render('AllocMariaFrontOfficeBundle:FrontOffice\BureauEncaissement:BureauEncaissement.html.twig');
+                    //return $this->redirectToRoute('espace_bureau_encaissement_alloc_maria_front_office_homepage');
+                }
+                elseif($libelle != NULL && $libelle != 'Encaissement'){
+                    echo "nok1";
+                    return $this->render('AllocMariaFrontOfficeBundle:FrontOffice:accueil.html.twig',array("form" => $form->createView(), "form2" => $form2->createView()));
+                }
+                else{
+                    echo "ok1";
+                    return $this->render('AllocMariaFrontOfficeBundle:FrontOffice:accueil.html.twig',array("form" => $form->createView(), "form2" => $form2->createView()));
+                }
+            }
+        }
+        else
 
-        //return $this->render('AllocMariaFrontOfficeBundle:FrontOffice:accueil.html.twig');
+            return $this->render('AllocMariaFrontOfficeBundle:FrontOffice:accueil.html.twig',array("form" => $form->createView(), "form2" => $form2->createView()));
+
     }
 
     public function BureauSectionsAction()
     {
-        $ress = "";
-        return $this->render('AllocMariaFrontOfficeBundle:FrontOffice\BureauDesSections:BureauSections.html.twig',array("ress"=>$ress));
+
+
+        return $this->render('AllocMariaFrontOfficeBundle:FrontOffice\BureauDesSections:BureauSections.html.twig',array('session'=> $_SESSION));
     }
 
     public function NouvelAdherentAction(Request $request)
@@ -167,7 +196,7 @@ class FrontofficeController extends Controller
             }
             $res = $query->getResult();
 
-            return $this->render('AllocMariaFrontOfficeBundle:FrontOffice\BureauDesSections:ListeAdherent.html.twig',array("adherents"=>$res, "form" => $form->createView()));
+            return $this->render('AllocMariaFrontOfficeBundle:FrontOffice\BureauDesSections:ListeAdherent.html.twig',array('session'=> $_SESSION, "adherents"=>$res, "form" => $form->createView()));
         }else
         {
             $repository = $this
@@ -179,7 +208,7 @@ class FrontofficeController extends Controller
 
 
 
-            return $this->render('AllocMariaFrontOfficeBundle:FrontOffice\BureauDesSections:ListeAdherent.html.twig',array("adherents"=>$listAdverts, "form" => $form->createView(),));
+            return $this->render('AllocMariaFrontOfficeBundle:FrontOffice\BureauDesSections:ListeAdherent.html.twig',array('session'=> $_SESSION, "adherents"=>$listAdverts, "form" => $form->createView(),));
 
         }
 
